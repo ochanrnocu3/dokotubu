54cd694febf5ee6e23e68b8a0c545744e543d817
Kaori MORI <kurnarnocu3@gmail.com>
2023-07-20 10:46:50 +0900

    使っていない箇所をコメントアウト

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -1,18 +1,7 @@
 package servlet;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import model.Account;
-import model.LoginLogic;
-import model.User;
 
 /**
  * Servlet implementation class LoginServlet
@@ -24,38 +13,33 @@ public class LoginServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// フォワード
-		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/login.jsp");
-		dispatcher.forward(request, response);
-	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// リクエストパラメータの取得
-		request.setCharacterEncoding("UTF-8");
-		String userId = request.getParameter("userId");
-		String pass = request.getParameter("pass");
-		
-		//ログイン処理の実行
-		User user = new User(userId,pass);
-		LoginLogic bo = new LoginLogic();
-		Account account = bo.execute(user);
-		
-		//ログイン処理の成否によって処理を分岐
-		if(account != null) {//ログイン成功時
-			//セッションスコープにユーザーIDを保存
-			HttpSession session = request.getSession();
-			session.setAttribute("userId", userId);
-			
-			//フォワード
-			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/loginOK.jsp");
-			dispatcher.forward(request, response);
-		} else { //ログイン失敗時
-			//リダイレクト
-			response.sendRedirect("LoginServlet");
-	}
-}
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// リクエストパラメータの取得
+//		request.setCharacterEncoding("UTF-8");
+//		String userId = request.getParameter("userId");
+//		String pass = request.getParameter("pass");
+//		
+//		//ログイン処理の実行
+//		User user = new User(userId,pass);
+//		LoginLogic bo = new LoginLogic();
+//		Account account = bo.execute(user);
+//		
+//		//ログイン処理の成否によって処理を分岐
+//		if(account != null) {//ログイン成功時
+//			//セッションスコープにユーザーIDを保存
+//			HttpSession session = request.getSession();
+//			session.setAttribute("userId", userId);
+//			
+//			//フォワード
+//			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/loginOK.jsp");
+//			dispatcher.forward(request, response);
+//		} else { //ログイン失敗時
+//			//リダイレクト
+//			response.sendRedirect("LoginServlet");
+//	}
 }
